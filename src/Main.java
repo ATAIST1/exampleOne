520362a36c38abf98d823eb6da7ce82ab0866605
Ернар <230709@astanait.edu.kz>
2023-12-14 10:22:39 +0600

    Code now prints total number of elements inputted before -1 and outputs all of them

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int[] arr = new int[1000];
+        int a = 0;
+        while(a != -1) {
+            a = sc.nextInt();
+            if(a != -1){
+                arr[count] = a;
+                count++;
+            }
+        }
+        System.out.println(count);
+        for(int i = 0; i < count; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
-        int a = sc.nextInt();
-        if(a%2==0)
-            System.out.println("Number is even");
-        else
-            System.out.println("Number is odd");
+
     }
 }
