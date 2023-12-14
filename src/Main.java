1af5966d67ff3653c74641a2698beba52950a85a
Ернар <230709@astanait.edu.kz>
2023-12-14 09:34:05 +0600

    Added logic to find out whether number is even or odd

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) { System.out.println("fffff");}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        if(a%2==0)
+            System.out.println("Number is even");
+        else
+            System.out.println("Number is odd");
+    }
 }
